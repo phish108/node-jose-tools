d006a51b0772e85a969ff4767b59515fe866245b
Christian P. Glahn <cpglahn@gmail.com>
2020-02-14 13:30:35 +0100

    newkeys test cases and corrections

--- test/02-newkey.js
+++ test/02-newkey.js
@@ -53,33 +53,294 @@ describe( "newkey tool tests", function() {
 
         expect(counter).to.be.equal(0);
         expect(result.length).not.equal(0);
+
+        jsonKey = JSON.parse(result);
+        expect(jsonKey).to.be.an("object");
+        expect(jsonKey).to.haveOwnProperty("kty");
+        expect(jsonKey.kty).to.be.equal("RSA");
+        
+        expect(jsonKey).to.haveOwnProperty("kid");
+
+        expect(jsonKey).to.haveOwnProperty("e");
+        expect(jsonKey.e).to.be.equal("AQAB");
+        expect(jsonKey).to.haveOwnProperty("n");
+        expect(jsonKey).to.haveOwnProperty("p");
+        expect(jsonKey).to.haveOwnProperty("q");
+        expect(jsonKey).to.haveOwnProperty("dp");
+        expect(jsonKey).to.haveOwnProperty("dq");
+        expect(jsonKey).to.haveOwnProperty("qi");
     });
 
-    it("plain new rsa 2048 key", async () => {
+    it("new rsa 2048 key as keystore", async () => {
+        let counter = 0;
 
+        let result = "";
+
+        try { 
+            result = await tool(["--type", "rsa", "-K"]);
+        }
+        catch (err) {
+            // this should complain about a missing type.
+            counter = 1;
+        }
+
+        expect(counter).to.be.equal(0);
+        expect(result.length).not.equal(0);
+
+        jsonKey = JSON.parse(result);
+        expect(jsonKey).to.be.an("object");
+        expect(jsonKey).to.haveOwnProperty("keys");
+        expect(jsonKey.keys).to.be.an("array");
+        expect(jsonKey.keys.length).to.be.equal(1);
+        expect(jsonKey.keys[0]).to.be.an("object");
+        expect(jsonKey.keys[0].kty).to.be.equal("RSA");
     });
 
-    it("new rsa 2048 key as keystore", async () => {});
+    it("new rsa 2048 key in existing keystore", async () => {
+        let counter = 0;
 
-    it("new rsa 2048 key in existing keystore", async () => {});
+        let result = "";
 
-    it("new rsa (bad) 1024 key ", async () => {});
+        try { 
+            result = await tool(["--type", "rsa", "-K", "-j", "examples/example-priv.jwks"]);
+        }
+        catch (err) {
+            // this should complain about a missing type.
+            counter = 1;
+        }
+
+        expect(counter).to.be.equal(0);
+        expect(result.length).not.equal(0);
+
+        jsonKey = JSON.parse(result);
+        expect(jsonKey).to.be.an("object");
+        expect(jsonKey).to.haveOwnProperty("keys");
+        expect(jsonKey.keys).to.be.an("array");
+        expect(jsonKey.keys.length).to.be.equal(4);
+    });
+
+    it("new rsa (bad) 1024 key ", async () => {
+        let counter = 0;
+
+        let result = "";
+
+        try { 
+            result = await tool(["--type", "rsa", "--size", "1024"]);
+        }
+        catch (err) {
+            // this should complain about a missing type.
+            counter = 1;
+            expect(err.message).to.be.equal("insufficient key length");
+        }
+
+        expect(counter).to.be.equal(1);
+    });
     
-    it("new rsa 4096 key ", async () => {});
+    it("new rsa 4096 key ", async () => {
+        let counter = 0;
 
-    it("new ec 256 key ", async () => {});
+        let result = "";
 
-    it("new oct 256 key ", async () => {});
+        try { 
+            result = await tool(["--type", "rsa", "--size", "4096"]);
+        }
+        catch (err) {
+            // this should complain about a missing type.
+            counter = 1;
+        }
 
-    it("new oct invalidly small key ", async () => {});
+        expect(counter).to.be.equal(0);
+        expect(result.length).not.equal(0);
 
-    it("new okp key ", async () => {});
+        jsonKey = JSON.parse(result);
+        expect(jsonKey).to.be.an("object");
+        expect(jsonKey).to.haveOwnProperty("kty");
+        expect(jsonKey.kty).to.be.equal("RSA");
+        
+        expect(jsonKey).to.haveOwnProperty("kid");
 
-    it("new dh key ", async () => {});
+        expect(jsonKey).to.haveOwnProperty("e");
+        expect(jsonKey.e).to.be.equal("AQAB");
+        expect(jsonKey).to.haveOwnProperty("n");
+        expect(jsonKey).to.haveOwnProperty("p");
+        expect(jsonKey).to.haveOwnProperty("q");
+        expect(jsonKey).to.haveOwnProperty("dp");
+        expect(jsonKey).to.haveOwnProperty("dq");
+        expect(jsonKey).to.haveOwnProperty("qi");
+    });
+
+    it("new oct 256 key ", async () => {
+        let counter = 0;
+
+        let result = "";
+
+        try { 
+            result = await tool(["--type", "oct", "--size", "256"]);
+        }
+        catch (err) {
+            // this should complain about a missing type.
+            counter = 1;
+        }
+
+        expect(counter).to.be.equal(0);
+        expect(result.length).not.equal(0);
+
+        jsonKey = JSON.parse(result);
+        expect(jsonKey).to.be.an("object");
+        expect(jsonKey).to.haveOwnProperty("kty");
+        expect(jsonKey.kty).to.be.equal("oct");
+        expect(jsonKey).to.haveOwnProperty("k");
+        expect(jsonKey).to.haveOwnProperty("kid");
+    });
+
+    it("new ec 256 key ", async () => {
+        let counter = 0;
+
+        let result = "";
+
+        try { 
+            result = await tool(["--type", "ec", "--size", "256"]);
+        }
+        catch (err) {
+            // this should complain about a missing type.
+            counter = 1;
+        }
+
+        expect(counter).to.be.equal(0);
+        expect(result.length).not.equal(0);
+
+        jsonKey = JSON.parse(result);
+        expect(jsonKey).to.be.an("object");
+        expect(jsonKey).to.haveOwnProperty("kty");
+        expect(jsonKey.kty).to.be.equal("EC");
+        expect(jsonKey).to.haveOwnProperty("crv", "P-256");
+        expect(jsonKey).to.haveOwnProperty("x");
+        expect(jsonKey).to.haveOwnProperty("y");
+        expect(jsonKey).to.haveOwnProperty("d");
+        expect(jsonKey).to.haveOwnProperty("kid");
+    });
+
+    it("new oct invalidly small key ", async () => {
+        let counter = 0;
+
+        let result = "";
+
+        try { 
+            result = await tool(["--type", "oct", "--size", "128"]);
+        }
+        catch (err) {
+            // this should complain about a missing type.
+            counter = 1;
+            expect(err.message).to.be.equal("insufficient key length");
+        }
+
+        expect(counter).to.be.equal(1);
+    });
+
+    it("new okp key ", async () => {
+        let counter = 0;
+
+        let result = "";
+
+        try { 
+            result = await tool(["--type", "okp", "--size", "448"]);
+        }
+        catch (err) {
+            // this should complain about a missing type.
+            counter = 1;
+            expect(err.message).to.be.equal("RFC8037 encryption and signatures are currently not supported");
+        }
+
+        expect(counter).to.be.equal(1);
+        // expect(counter).to.be.equal(0);
+        // expect(result.length).not.equal(0);
+
+        // jsonKey = JSON.parse(result);
+        // expect(jsonKey).to.be.an("object");
+        // expect(jsonKey).to.haveOwnProperty("kty");
+        // expect(jsonKey.kty).to.be.equal("OKP");
+        // console.log(result);
+    });
+
+    it("new key with beautify on", async () => {
+        let counter = 0;
+
+        let result = "";
+
+        try { 
+            result = await tool(["--type", "oct", "--size", "256", "-b"]);
+        }
+        catch (err) {
+            // this should complain about a missing type.
+            counter = 1;
+        }
+
+        expect(counter).to.be.equal(0);
+        expect(result.length).not.equal(0);
+        
+        let lines = result.split(/\r\n|\r|\n/).length;
+
+        expect(lines).to.be.equal(6)
+    });
+
+    it("new key for signing", async () => {
+        let counter = 0;
+
+        let result = "";
+
+        try { 
+            result = await tool(["--type", "oct", "--size", "256", "--use", "sign"]);
+        }
+        catch (err) {
+            // this should complain about a missing type.
+            counter = 1;
+        }
+
+        expect(counter).to.be.equal(0);
+        expect(result.length).not.equal(0);
+        jsonKey = JSON.parse(result);
+        expect(jsonKey).to.be.an("object");
+        expect(jsonKey).to.haveOwnProperty("use");
+        expect(jsonKey.use).to.be.equal("sig");
+    });
+
     
-    it("new key with beautify on", async () => {});
+    it("new key for encrypting", async () => {
+        let counter = 0;
 
-    it("new key for signing", async () => {});
+        let result = "";
 
-    it("new key for encrypting", async () => {});
+        try { 
+            result = await tool(["--type", "oct", "--size", "256", "--use", "enc"]);
+        }
+        catch (err) {
+            // this should complain about a missing type.
+            counter = 1;
+        }
+
+        expect(counter).to.be.equal(0);
+        expect(result.length).not.equal(0);
+        jsonKey = JSON.parse(result);
+        expect(jsonKey).to.be.an("object");
+        expect(jsonKey).to.haveOwnProperty("use");
+        expect(jsonKey.use).to.be.equal("enc");
+    });
+
+    it("new key for bad usage", async () => {
+        let counter = 0;
+
+        let result = "";
+
+        try { 
+            result = await tool(["--type", "oct", "--size", "256", "--use", "baz"]);
+        }
+        catch (err) {
+            // this should complain about a missing type.
+            counter = 1;
+            expect(err.message).to.be.equal("unknown use baz")
+        }
+
+        expect(counter).to.be.equal(1);
+    });
+
 } );
