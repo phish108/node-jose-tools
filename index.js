c7384870fa516498470b124ee5f75d5f1e14391b
Christian P. Glahn <cpglahn@gmail.com>
2020-02-14 12:28:47 +0100

    make tools testable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 require(`./lib/${process.argv[2]}.js`)(process.argv.slice(3))
-    .then(() => process.exit(0))
+    .then((data) => process.stdout.write(data, "utf8"))
     .catch((err) => {
         // process.stderr.write(err.message);
         console.log(err); // eslint-disable-line no-console
--- test/02-newkey.js
+++ test/02-newkey.js
@@ -4,9 +4,60 @@
 const chai = require("chai");
 const expect = chai.expect;
 
+const tool = require("../lib/newkey.js");
+
 describe( "newkey tool tests", function() {
 
-    it("plain new rsa 2048 key", async () => {});
+    it("no parameters", async () => {
+        let counter = 0;
+
+        try {
+            await tool([]);
+        }
+        catch (err) {
+            // this should complain about a missing type.
+            counter = 1;
+            expect(err.message).to.be.equal("missing type");
+        }
+
+        expect(counter).to.be.equal(1);
+    });
+
+    it("just type parameter, invalid type", async () => {
+        let counter = 0;
+
+        try {
+            await tool(["--type", "foo"]);
+        }
+        catch (err) {
+            // this should complain about a missing type.
+            counter = 1;
+            expect(err.message).to.be.equal("unknown key type FOO");
+        }
+
+        expect(counter).to.be.equal(1);
+    });
+
+    it("just type parameter, valid type without size", async () => {
+        let counter = 0;
+
+        let result = "";
+
+        try { 
+            result = await tool(["--type", "rsa"]);
+        }
+        catch (err) {
+            // this should complain about a missing type.
+            counter = 1;
+        }
+
+        expect(counter).to.be.equal(0);
+        expect(result.length).not.equal(0);
+    });
+
+    it("plain new rsa 2048 key", async () => {
+
+    });
 
     it("new rsa 2048 key as keystore", async () => {});
 
