80cf589daac4b6203f1023cffe1a40348fc2463a
Christian Glahn <cpglahn@gmail.com>
2020-02-15 11:52:36 +0100

    addkey and list key tests, fixes, and test data

--- test/03-addkey.js
+++ test/03-addkey.js
@@ -1,17 +1,232 @@
 /* eslint-env node, mocha */
 /* eslint-disable require-jsdoc */
 
+const fs = require("fs");
 const chai = require("chai");
 const expect = chai.expect;
 
+const tool = require("../lib/addkey.js");
+
 describe( "addkey tool tests", function() {
-    it("load key from json", async () => {});
-    it("load key from pem", async () => {});
-    it("load key from pkcs8", async () => {});
-    it("load key from x509", async () => {});
-    it("load key from pkix", async () => {});
-    it("load key from spki", async () => {});
-    it("update keystore with new file", async () => {});
-    it("create and store keystore with new file", async () => {});
-    it("load key and beautify", async () => {});
-} );
+    const filestore = "test/files/";
+
+    it("load key from json", async () => {
+        let result, count = 0;
+
+        try {
+            result = await tool(["-j", filestore + "empty.jwks",
+                filestore + "single_key.jwk"]);
+        }
+        catch (err) {
+            count += 1;
+            console.log(err.message);
+        }
+
+        expect(count).to.equal(0);
+
+        const jsonKS = JSON.parse(result);
+
+        expect(jsonKS).to.be.an("object");
+        expect(jsonKS).to.own.property("keys");
+        expect(jsonKS.keys).to.have.length(1);
+    });
+
+    it.skip("load key from pem", async () => {
+
+    });
+
+    it.skip("load key from pkcs8", async () => {
+
+    });
+
+    it.skip("load key from x509", async () => {
+
+    });
+
+    it.skip("load key from pkix", async () => {
+
+    });
+
+    it.skip("load key from spki", async () => {
+
+    });
+
+    it("add multiple individual keys", async () => {
+        let result, count = 0;
+
+        try {
+            result = await tool(["-j", filestore + "empty.jwks",
+                filestore + "single_key.jwk",
+                filestore + "octet_key.jwk"]);
+        }
+        catch (err) {
+            count += 1;
+            console.log(err.message);
+        }
+
+        expect(count).to.equal(0);
+
+        const jsonKS = JSON.parse(result);
+
+        expect(jsonKS).to.be.an("object");
+        expect(jsonKS).to.own.property("keys");
+        expect(jsonKS.keys).to.have.length(2);
+
+        // The order is based on timing, not on position in the arguments
+        expect(jsonKS.keys[0].kty).to.equal("RSA");
+        expect(jsonKS.keys[1].kty).to.equal("oct");
+    });
+
+    it("add multiple keys by merging keystores", async () => {
+        let result, count = 0;
+
+        try {
+            result = await tool(["-j", filestore + "testing_rsa.jwks",
+                filestore + "testing_octets.jwks"]);
+        }
+        catch (err) {
+            count += 1;
+            console.log(err.message);
+        }
+
+        expect(count).to.equal(0);
+
+        const jsonKS = JSON.parse(result);
+
+        expect(jsonKS).to.be.an("object");
+        expect(jsonKS).to.own.property("keys");
+        expect(jsonKS.keys).to.have.length(2);
+        expect(jsonKS.keys[0].kty).to.equal("RSA");
+        expect(jsonKS.keys[1].kty).to.equal("oct");
+    });
+
+    it.skip("add multiple keys by merging keystores with overlapping keys", async () => {
+
+    });
+
+    it("add multiple keys by merging multiple keystores", async () => {
+        let result, count = 0;
+
+        try {
+            result = await tool(["-j", filestore + "testing_rsa.jwks",
+                filestore + "testing_octets.jwks",
+                filestore + "testing_rsa2.jwks"]);
+        }
+        catch (err) {
+            count += 1;
+            console.log(err.message);
+        }
+
+        expect(count).to.equal(0);
+
+        const jsonKS = JSON.parse(result);
+
+        expect(jsonKS).to.be.an("object");
+        expect(jsonKS).to.own.property("keys");
+        expect(jsonKS.keys).to.have.length(3);
+        expect(jsonKS.keys[0].kty).to.equal("RSA");
+
+        // this order is based on timing, not on argument position
+        expect(jsonKS.keys[1].kty).to.equal("oct");
+        expect(jsonKS.keys[2].kty).to.equal("RSA");
+    });
+
+    it.skip("add multiple keys by merging multiple keystores with overlapping keys", async () => {
+
+    });
+
+
+    describe("addkey tool fs updates", async () => {
+        const newkeyTool = require("../lib/newkey");
+        const filehelper = require("../lib/helper/readfile");
+        const datastore = "tmp_store.jwks";
+
+        before(async () => {
+            await newkeyTool(["-r", "-U", "-j", filestore + datastore]);
+        });
+
+        after(() => {
+            fs.unlinkSync(filestore + datastore);
+            fs.unlinkSync(filestore + "tmp_newkey.jwks");
+            // fs.unlinkSync(filestore + "tmp_newkey2.jwks");
+        });
+
+        it("update keystore with new key", async () => {
+            let count = 0;
+
+            try {
+                await tool(["-U", "-j", filestore + datastore,
+                    filestore + "testing_octets.jwks"]);
+            }
+            catch (err) {
+                count += 1;
+                console.log(err.message);
+            }
+
+            expect(count).to.equal(0);
+
+            // open the file store
+            const jwksdata = await filehelper.loadFile(filestore + datastore);
+
+            const jsonKS = JSON.parse(jwksdata);
+
+            expect(jsonKS).to.be.an("object");
+            expect(jsonKS).to.own.property("keys");
+
+            expect(jsonKS.keys).to.have.length(2);
+            expect(jsonKS.keys[0].kty).to.equal("RSA");
+            expect(jsonKS.keys[1].kty).to.equal("oct");
+        });
+
+        it("create and store keystore with new key", async () => {
+            let count = 0;
+
+            try {
+                await tool(["-U", "-C", "-j", filestore + "tmp_newkey.jwks",
+                    filestore + "testing_octets.jwks"]);
+            }
+            catch (err) {
+                count += 1;
+                console.log(err.message);
+            }
+
+            expect(count).to.equal(0);
+
+            // open the file store
+            const jwksdata = await filehelper.loadFile(filestore + "tmp_newkey.jwks");
+
+            const jsonKS = JSON.parse(jwksdata);
+
+            expect(jsonKS).to.be.an("object");
+            expect(jsonKS).to.own.property("keys");
+
+            expect(jsonKS.keys).to.have.length(1);
+            expect(jsonKS.keys[0].kty).to.equal("oct");
+        });
+
+        it("store in non-existing keystore with new key", async () => {
+            let count = 0;
+
+            try {
+                await tool(["-U", "-j", filestore + "tmp_newkey2.jwks",
+                    filestore + "testing_octets.jwks"]);
+            }
+            catch (err) {
+                count += 1;
+                // console.log(err.message);
+            }
+
+            expect(count).to.equal(1);
+
+            // open the file store
+            // const jwksdata = await filehelper.loadFile(filestore + "tmp_newkey2.jwks");
+
+            // const jsonKS = JSON.parse(jwksdata);
+
+            // expect(jsonKS).to.be.an("object");
+            // expect(jsonKS).to.own.property("keys");
+
+            // expect(jsonKS.keys).to.have.length(0);
+        });
+    });
+});
