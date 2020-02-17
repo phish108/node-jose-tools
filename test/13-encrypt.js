4421da3f9c64a76db2f95e776f0eea680b5c2802
Christian Glahn <cpglahn@gmail.com>
2020-02-17 21:36:36 +0100

    prepare excryption tests

--- test/13-encrypt.js
+++ test/13-encrypt.js
@@ -4,16 +4,44 @@
 const chai = require("chai");
 const expect = chai.expect;
 
+const tool = require("../lib/encrypt");
+
 describe( "encrypt tool tests", function() {
-    it.skip("encrypt for aud", async () => {});
+    const pubkeys = "examples/example-pub.jwks";
 
-    it.skip("encrypt for aud with kid", async () => {});
-    it.skip("encrypt for aud with kid and alg", async () => {});
-    it.skip("encrypt with enc", async () => {});
-    it.skip("encrypt to general", async () => {});
-    it.skip("encrypt to flattened", async () => {});
-    it.skip("encrypt to compact", async () => {});
+    it.skip("encrypt without parameters", async () => {
 
-    it.skip("encrypt without key", async () => {});
-    it.skip("encrypt with empty payload", async () => {});
-} );
+    });
+
+    it.skip("encrypt for aud with kid", async () => {
+
+    });
+
+    it.skip("encrypt for aud with kid and alg", async () => {
+
+    });
+
+    it.skip("encrypt with enc", async () => {
+
+    });
+
+    it.skip("encrypt to general", async () => {
+
+    });
+
+    it.skip("encrypt to flattened", async () => {
+
+    });
+
+    it.skip("encrypt to compact", async () => {
+
+    });
+
+    it.skip("encrypt without key", async () => {
+
+    });
+
+    it.skip("encrypt with empty payload", async () => {
+
+    });
+});
--- test/14-decrypt.js
+++ test/14-decrypt.js
@@ -4,11 +4,22 @@
 const chai = require("chai");
 const expect = chai.expect;
 
+const encrypttool = require("../lib/encrypt");
+const tool = require("../lib/decrypt");
+
 describe( "decrypt tool tests", function() {
+    const pubkeys = "examples/example-pub.jwks";
+    const prvkeys = "examples/example-priv.jwks";
+
+    it.skip("decrypt without parameters", async () => {});
+
     it.skip("decrypt with key", async () => {});
-    it.skip("decrypt with kid", async () => {});
+
     it.skip("decrypt without key", async () => {});
+
     it.skip("decrypt with bad kid", async () => {});
+
     it.skip("decrypt with kid to public key", async () => {});
+
     it.skip("enforce crit headers", async () => {});
 } );
