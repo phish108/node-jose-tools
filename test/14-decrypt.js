e57ddc8252c0b4b6e21a177471bf3809b805c509
Christian Glahn <cpglahn@gmail.com>
2020-02-23 14:43:44 +0100

    add decrypt test stubs

--- test/14-decrypt.js
+++ test/14-decrypt.js
@@ -10,16 +10,190 @@ const tool = require("../lib/decrypt");
 describe( "decrypt tool tests", function() {
     const pubkeys = "examples/example-pub.jwks";
     const prvkeys = "examples/example-priv.jwks";
+    const tmpkeys = "test/files/tmp-keystore.jwks";
 
-    it.skip("decrypt without parameters", async () => {});
+    // create temporary keystore and RSA and EC Keys
+    before(async () => {});
 
-    it.skip("decrypt with key", async () => {});
+    // remove temporary keystore
+    after(async () => {});
 
-    it.skip("decrypt without key", async () => {});
+    it.skip("decrypt without parameters", async () => {
+        let result, token, count = 0;
 
-    it.skip("decrypt with bad kid", async () => {});
+        try {
+            result = tool([]);
+        }
+        catch (err) {
+            count += 1;
+            expect(err.message).to.equal("no options provided");
+        }
+        
+        expect(count).to.be.equal(1);
+    });
 
-    it.skip("decrypt with kid to public key", async () => {});
+    it.skip("dir decrypt with keystore", async () => {
+        let result, token, count = 0;
+        const alg = "dir";
+        // const enc;
+        const kid = "foobar";
+        const payload = "hello world";
 
-    it.skip("enforce crit headers", async () => {});
+        token = await encrypttool([
+
+        ]);
+
+        try {
+            result = tool([
+                
+            ]);
+        }
+        catch (err) {
+            count += 1;
+            console.log(err.message);
+        }
+        
+        expect(count).to.be.equal(0);
+    });
+
+    // for this test a temporary key store is needed
+    it.skip("decrypt with bad kid", async () => {
+        let result, token, count = 0;
+        const alg = "dir";
+        const kid = "foobaz";
+        const payload = "hello world";
+
+        token = await encrypttool([
+
+        ]);
+
+        try {
+            result = tool([
+
+            ]);
+        }
+        catch (err) {
+            count += 1;
+            console.log(err.message);
+        }
+
+        expect(count).to.be.equal(1);
+    });
+
+    it.skip("rsa decrypt", async () => {
+        let result, token, count = 0;
+        const alg = "RSA-OAEP";
+        const kid = "foorsa";
+        const payload = "hello world";
+
+        token = await encrypttool([
+
+        ]);
+
+        try {
+            result = tool([
+
+            ]);
+        }
+        catch (err) {
+            count += 1;
+            console.log(err.message);
+        }
+
+        expect(count).to.be.equal(0);
+    });
+
+
+    it.skip("rsa decrypt without payload", async () => {
+        let result, token, count = 0;
+        const alg = "RSA-OAEP";
+        const kid = "foorsa";
+        // const payload = "hello world";
+
+        token = await encrypttool([
+
+        ]);
+
+        try {
+            result = tool([
+
+            ]);
+        }
+        catch (err) {
+            count += 1;
+            console.log(err.message);
+        }
+
+        expect(count).to.be.equal(0);
+
+    });
+
+    it.skip("rsa decrypt with kid to public key", async () => {
+        let result, token, count = 0;
+        const alg = "RSA-OAEP";
+        const kid = "foorsa";
+        const payload = "hello world";
+
+        token = await encrypttool([
+
+        ]);
+
+        try {
+            result = tool([
+
+            ]);
+        }
+        catch (err) {
+            count += 1;
+            console.log(err.message);
+        }
+
+        expect(count).to.be.equal(0);
+    });
+
+    it.skip("enforce crit headers on the receiver", async () => {
+        let result, token, count = 0;
+        const alg = "RSA-OAEP";
+        const kid = "foorsa";
+        const payload = "hello world";
+
+        token = await encrypttool([
+
+        ]);
+
+        try {
+            result = tool([
+
+            ]);
+        }
+        catch (err) {
+            count += 1;
+            console.log(err.message);
+        }
+
+        expect(count).to.be.equal(0);
+    });
+
+    it.skip("include crit headers on the sender", async () => {
+        let result, token, count = 0;
+        const alg = "RSA-OAEP";
+        const kid = "foorsa";
+        const payload = "hello world";
+
+        token = await encrypttool([
+
+        ]);
+
+        try {
+            result = tool([
+
+            ]);
+        }
+        catch (err) {
+            count += 1;
+            console.log(err.message);
+        }
+
+        expect(count).to.be.equal(0);
+    });
 } );
