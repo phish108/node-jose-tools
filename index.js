2e7f7e54c7856684d7963ef62aeda161cb070e8a
Christian P. Glahn <cpglahn@gmail.com>
2020-02-25 21:53:26 +0100

    sanitize name and tests

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
 #!/usr/bin/env node
 
-if (process.argv[2] === undefined) {
-    console.log("ERROR: No tool selected.");
-    process.exit(1);
-}
-
-// verify that the requested too actually exists.
-const fs = require("fs");
-
 // drop all dots and slashes
-let tool = process.argv[2]; 
-tool = tool.replace(/\./g, ""); // drop all dots
-tool = tool.replace(/\//g, "-"); // replace all slashed with dashes
-
-let file = null; 
-
-try {
-    file = fs.statSync(`./lib/${tool}.js`);
-}
-catch (err) {
-    console.log("ERROR: invalid tool selected.");
-    process.exit(1);
-}
-
-if (!file.isFile()) {
-    console.log("ERROR: invalid tool selected.");
-    process.exit(1);
-}
-
-
-require(`./lib/${tool}.js`)(process.argv.slice(3))
+require("./lib/helper/sanitize.js")(process.argv[2])
+    // include and run the tool
+    .then((tool) => require(`./lib/${tool}`)(process.argv.slice(3)))
+    // print on success
     .then((data) => process.stdout.write(data, "utf8"))
+    // gracefully fail on error
     .catch((err) => {
         // process.stderr.write(err.message);
-        console.log(err); // eslint-disable-line no-console
+        console.log(err.message); // eslint-disable-line no-console
         process.exit(1);
     });
--- test/00-sanitize-tools.js
+++ test/00-sanitize-tools.js
@@ -4,21 +4,147 @@
 const chai = require("chai");
 const expect = chai.expect;
 
+const tool = require("../lib/helper/sanitize");
 
 describe( "sanitize tool names", function() {
-    it.skip("valid tool name", async () => {
+    it("undefined toolname", async () => {
+        let count = 0;
 
+        try {
+            await tool();
+        }
+        catch (err) {
+            count += 1;
+            expect(err.message).to.equal("missing toolname");
+        }
+        expect(count).to.equal(1);
     });
 
-    it.skip("empty tool name", async () => {
+    it("empty toolname", async () => {
+        let count = 0;
 
+        try {
+            await tool("");
+        }
+        catch (err) {
+            count += 1;
+            expect(err.message).to.equal("missing toolname");
+        }
+        expect(count).to.equal(1);
     });
 
-    it.skip("dotted tool name", async () => {
+    it("valid tool name", async () => {
+        let result, count = 0;
 
+        try {
+            result =  await tool("addkey");
+        }
+        catch (err) {
+            count += 1;
+            console.log(err.message);
+        }
+
+        expect(count).to.equal(0);
+        expect(result).to.equal("addkey");
     });
 
-    it.skip("pathlike tool name", async () => {
+    it("dotted tool name", async () => {
+        let result, count = 0;
 
+        try {
+            result =  await tool("add.key");
+        }
+        catch (err) {
+            count += 1;
+            console.log(err.message);
+        }
+
+        expect(count).to.equal(0);
+        expect(result).to.equal("addkey");
+    });
+
+    it("pathlike tool name", async () => {
+        let result, count = 0;
+
+        try {
+            result =  await tool("no/op");
+        }
+        catch (err) {
+            count += 1;
+            console.log(err.message);
+        }
+
+        expect(count).to.equal(0);
+        expect(result).to.equal("no-op");
+    });
+
+    it("invalid non existing tool name", async () => {
+        let count = 0;
+
+        try {
+            await tool("missing");
+        }
+        catch (err) {
+            count += 1;
+            expect(err.message).to.equal("unknown tool name");
+        }
+
+        expect(count).to.equal(1);
+    });
+
+    it("invalid tool name for existing path", async () => {
+        let count = 0;
+
+        try {
+            await tool("helper");
+        }
+        catch (err) {
+            count += 1;
+            expect(err.message).to.equal("unknown tool name");
+        }
+
+        expect(count).to.equal(1);
+    });
+
+    it("invalid pathlike tool name", async () => {
+        let count = 0;
+
+        try {
+            await tool("helper/readfile");
+        }
+        catch (err) {
+            count += 1;
+            expect(err.message).to.equal("unknown tool name");
+        }
+
+        expect(count).to.equal(1);
+    });
+
+    it("invalid dotted tool name", async () => {
+        let count = 0;
+
+        try {
+            await tool("addkey.js");
+        }
+        catch (err) {
+            count += 1;
+            expect(err.message).to.equal("unknown tool name");
+        }
+
+        expect(count).to.equal(1);
+    });
+
+    it("invalid parent pathlike tool name", async () => {
+        let count = 0;
+
+        try {
+            await tool("../../lib/addkey");
+        }
+        catch (err) {
+            count += 1;
+            expect(err.message).to.equal("unknown tool name");
+        }
+
+        expect(count).to.equal(1);
     });
 });
